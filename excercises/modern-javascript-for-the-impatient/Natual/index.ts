999887aaf47502aeef30f4be8ba912f803d34ebe
seiya <20365512+seiyab@users.noreply.github.com>
2022-05-14 07:28:38 +0900

    Natural

---
+++ excercises/modern-javascript-for-the-impatient/Natual/index.ts
@@ -0,0 +1,6 @@
+export class Natural {
+    static [Symbol.hasInstance](obj: unknown) {
+        if (typeof obj !== 'number') return false
+        return obj > 0 && Math.floor(obj) === obj
+    }
+}
